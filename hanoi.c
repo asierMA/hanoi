3e20942f673c2c2dd08bd382189e2e46cf1e4802
asierMA <99725624+asierMA@users.noreply.github.com>
2023-10-02 12:58:06 +0200

    Update hanoi.c
    I have fixed it

--- hanoi.c
+++ hanoi.c
@@ -3,11 +3,11 @@
 void hanoi(int n, int a, int b)
 {
   if (n == 1) 
-    printf("Presuňte kameň 1 z %d na %d\n", a, b);
+    printf("Move stone 1 from %d to %d\n", a, b);
   else
   {
     hanoi(n - 1, a, 6 - a - b);
-    printf("Presuňte kameň %d z %d na %d\n", n, a, b);
+    printf("Move stone %d from %d to %d\n", n, a, b);
     hanoi(n - 1, 6 - a - b, b);
   }
 }
